2c12bb8548ad2425ec56cff10e1e645fc895e234
Zafar Ali Khan <33575935+zakonweb@users.noreply.github.com>
2019-09-14 09:42:58 +0500

    Q4 of 9608_s19_ms_21
    Pseudocode resolved in VB in non functional/procedural form.

---
+++ Q4 9608_s19_ms_21.vb
@@ -0,0 +1,30 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim InString As String = "  X   Y and  Z"
+        Dim NewString As String = ""
+        Dim Index As Integer = 0
+        Dim AfterSpace As Boolean = False
+        Dim NextChar As String = ""
+        Const Space = " "
+
+        For Index = 1 To Len(InString)
+            NextChar = Mid(InString, Index, 1)
+            If AfterSpace = True Then
+                If NextChar <> Space Then
+                    NewString = NewString & NextChar
+                    AfterSpace = False
+                End If
+            Else
+                NewString = NewString & NextChar
+                If NextChar = Space Then
+                    AfterSpace = True
+                End If
+            End If
+        Next
+
+        Console.WriteLine(NewString)
+        Console.ReadKey()
+    End Sub
+
+End Module
