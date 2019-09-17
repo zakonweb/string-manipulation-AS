b243c10be72339f899857b9383884e574b0dc763
Zafar Ali Khan <33575935+zakonweb@users.noreply.github.com>
2019-09-17 15:51:39 +0500

    Add files via upload

---
+++ myFirstApp.vb
@@ -0,0 +1,20 @@
+﻿Module Module1
+    Sub Main()
+        Dim myPer As Single
+        myPer = 0
+
+        Dim myName As String
+        myName = ""
+
+        Dim counter As Integer
+        counter = 0
+
+        Console.Write("Enter Name: ")
+        myName = Console.ReadLine
+
+        Console.WriteLine(myName)
+        
+        Console.WriteLine("Press any key to continue...")
+        Console.ReadKey()
+    End Sub
+End Module
