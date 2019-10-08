18647e5a5e98b4ebbc6e2080bdcf5e59f6c16151
Zafar Ali Khan <33575935+zakonweb@users.noreply.github.com>
2019-10-08 15:39:24 +0500

    Pangram VB source code 2.

---
+++ Pangram2.vb
@@ -0,0 +1,38 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim myStr As String
+        Dim a, b As Integer
+        Dim isFound As Boolean = False
+        Dim ch As Char
+
+        Console.Write("Enter phrase to check Pangram: ")
+        myStr = Console.ReadLine
+        If Len(myStr) < 26 Then
+            Console.WriteLine("It is not Pangram.")
+            Console.ReadKey()
+        Else
+            For b = 65 To 90
+                isFound = False
+                For a = 1 To Len(myStr)
+                    ch = UCase(Mid(myStr, a, 1))
+                    If Asc(ch) = b Then
+                        isFound = True
+                        Exit For
+                    End If
+                Next
+                If isFound = False Then
+                    Console.WriteLine("It is not Pangram.")
+                    Console.ReadKey()
+                    Exit For
+                End If
+            Next b
+            If isFound = True Then
+                Console.WriteLine("It is a Pangram.")
+                Console.ReadKey()
+            End If
+        End If
+    End Sub
+
+
+End Module
