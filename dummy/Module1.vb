258253c8ff8f5da7ccdf84f94fe729cc09939f07
Zafar Ali Khan <zakonweb@gmail.com>
2022-11-18 15:22:30 +0500

    dummy code

---
+++ dummy/Module1.vb
@@ -0,0 +1,20 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim x, b As String
+        Dim a, c As Integer
+        Dim thisChar As Char
+
+        x = "ZAFAR ALI KHAN"
+        b = "AEIOU"
+
+        For c = 1 To Len(x)
+            thisChar = Mid(x, c, 1)
+            a = Asc(thisChar)
+            Console.WriteLine(thisChar & " = " & a)
+        Next
+
+        Console.ReadKey()
+    End Sub
+
+End Module
